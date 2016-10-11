d75c9eb7ed4bfaeb41c136935e7cd3b1337df777
gambastian <sebas.gamba02@gmail.com>
2016-10-11 09:04:55 -0500

    Rojo Paso 2 prueba de registro fallida

--- polls/Functional_test.py
+++ polls/Functional_test.py
@@ -1,19 +1,68 @@
 from unittest import TestCase
+
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# from buscoayuda4101 import settings
+# from polls.models import TiposDeServicio
+
 
 class FunctionalTest(TestCase):
-
+    
+    # SERVICE_TYPE = TiposDeServicio(nombre='Reporter')
+    
     def setUp(self):
+        # settings.configure()
+        # self.SERVICE_TYPE.save()
         prof = webdriver.FirefoxProfile()
         prof.set_preference('browser.startup.homepage', 'about:blank')
         prof.set_preference('startup.homepage_welcome_url', 'about:blank')
         prof.set_preference('startup.homepage_welcome_url.additional', 'about:blank')
         prof.set_preference('browser.startup.homepage_override.mstone', 'ignore')
         self.browser = webdriver.Firefox(prof)
+        
 
     def tearDown(self):
+        # self.SERVICE_TYPE.delete()
         self.browser.quit()
 
     def test_title(self):
         self.browser.get("http://localhost:8000")
         self.assertIn('Busco Ayuda', self.browser.title)
+
+    def test_register_user(self):
+        self.browser.get("http://localhost:8000")
+        link = self.browser.find_element_by_id('id_register');
+        link.click()
+
+        nombre = self.browser.find_element_by_id('id_nombre')
+        nombre.send_keys('Peter')
+
+        apellidos = self.browser.find_element_by_id('id_apellidos')
+        apellidos.send_keys('Parker')
+
+        aniosExperiencia = self.browser.find_element_by_id('id_aniosExperiencia')
+        aniosExperiencia.send_keys(10)
+
+        self.browser.find_element_by_xpath("//select[@id='id_tiposDeServicio']/option[text()='Reporter']").click()
+
+        telefono = self.browser.find_element_by_id('id_telefono')
+        telefono.send_keys(3111234)
+
+        correo = self.browser.find_element_by_id('id_correo')
+        correo.send_keys('test@mail.fake')
+
+        username = self.browser.find_element_by_id('id_username' + str(int(round(time.time() * 1000))))
+        username.send_keys('username')
+
+        password = self.browser.find_element_by_id('id_password')
+        password.send_keys('password')
+
+        saveButton = self.browser.find_element_by_id('id_grabar')
+        saveButton.click()
+
+        self.browser.implicitly_wait(3)
+        span = self.browser.find_element(By.XPATH, "//span[text()='Peter Parker']")
+
+        self.assertIn('Peter Parker', span.text)
